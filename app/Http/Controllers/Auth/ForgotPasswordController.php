132576bdf2d2db7bde0fcd1788cd6fe30eecdba5
Emmanuel Lanuzo <emmanuellanuzo@gmail.com>
2018-08-18 19:05:01 +0800

    Start From the Scratch

--- app/Http/Controllers/Auth/ForgotPasswordController.php
+++ app/Http/Controllers/Auth/ForgotPasswordController.php
@@ -1,8 +1,8 @@
 <?php
 
-namespace WebCTF\Http\Controllers\Auth;
+namespace App\Http\Controllers\Auth;
 
-use WebCTF\Http\Controllers\Controller;
+use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\SendsPasswordResetEmails;
 
 class ForgotPasswordController extends Controller
--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace WebCTF\Providers;
+namespace App\Providers;
 
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
@@ -13,7 +13,7 @@ class AuthServiceProvider extends ServiceProvider
      * @var array
      */
     protected $policies = [
-        'WebCTF\Model' => 'WebCTF\Policies\ModelPolicy',
+        'App\Model' => 'App\Policies\ModelPolicy',
     ];
 
     /**
